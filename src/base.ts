22e388d295f17deb06b2cb25b95338538feb9881
wxn0brP <viviennevaltheris@gmail.com>
2026-09-22 18:19:14 +0200

    fix: browser support

--- src/base.ts
+++ src/base.ts
@@ -24,8 +24,8 @@ export class BaseRemote {
 		};
 
 		url.pathname = url.pathname.replace(/\/$/, "") + "/db/" + type;
-		delete url.username;
-		delete url.password;
+		url.username = "";
+		url.password = "";
 
 		const res = (await fetch(url, {
 			method: "POST",
